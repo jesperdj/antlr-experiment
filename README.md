08ca5663f9d7ba2b8322c2fce16262f5adce46bb
Jesper de Jong <jespdj@gmail.com>
2014-11-23 19:36:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trying out ANTLR4
+
+Small project for experimenting with [ANTLR4](http://www.antlr.org/).
+
+See the book: [The Definitive ANTLR4 Reference](https://pragprog.com/book/tpantlr2/the-definitive-antlr-4-reference).
---
+++ src/main/java/com/jesperdj/antlr/arithmetic/Main.java
@@ -0,0 +1,24 @@
+package com.jesperdj.antlr.arithmetic;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public final class Main {
+
+    public static void main(String[] args) throws IOException {
+        final InputStream in = args.length > 0 ? new FileInputStream(args[0]) : System.in;
+
+        final ArithmeticLexer lexer = new ArithmeticLexer(new ANTLRInputStream(in));
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final ArithmeticParser parser = new ArithmeticParser(tokens);
+
+        final ParseTree tree = parser.prog();
+
+        System.out.println(tree.toStringTree(parser));
+    }
+}
